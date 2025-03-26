7f246007984d9e0471104d17b8743b35b2db23c4
Henry <henryyanggg@gmail.com>
2025-03-26 11:59:01 +0700

    Gemini timeout to 30s

--- src/config/geminiAxios.js
+++ src/config/geminiAxios.js
@@ -6,7 +6,7 @@ const geminiClient = axios.create({
     headers: {
         "Content-Type": "application/json"
     },
-    timeout: 5000,
+    timeout: 30000,
 });
 
 geminiClient.interceptors.request.use(
